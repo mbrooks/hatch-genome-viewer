c71e6ac347c96f254ce132951d43c93a3d8bcedc
Matt Brooks <matt@msbrooks.com>
2019-01-30 07:43:35 -0600

    Massaging eslint rules
    Massaing eslint rules to turn off silly error messages that don't
    really make sense for a react app.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
         ],
         "react/prefer-stateless-function": [0],
         "react/jsx-filename-extension": [0],
-        "jsx-a11y/click-events-have-key-events":[0]
+        "jsx-a11y/click-events-have-key-events": [0],
+        "jsx-a11y/no-static-element-interactions": [0]
     }
 };
